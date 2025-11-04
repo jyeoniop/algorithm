9d898054e8af3376662bcc0adb85a434d46bc66f
jyeoniop <wldud128iop@gmail.com>
2025-11-04 18:00:32 +0900

    [Bronze V] Title: A+B - 3, Time: 188 ms, Memory: 17904 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,21 @@
+import java.util.*;
+// import java.time.LocalDate;
+
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+        int T = sc.nextInt();
+
+
+        int result = 1;
+
+        for(int i=0;i<T;i++){
+
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a+b);
+        }
+        
+    }
+}
