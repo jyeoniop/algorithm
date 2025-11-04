ad160b1557650fddd9c8ce5b4ad65410f313f62e
jyeoniop <wldud128iop@gmail.com>
2025-11-04 17:03:03 +0900

    [Bronze V] Title: 등록, Time: 96 ms, Memory: 13744 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.time.LocalDate;
+
+public class Main{
+    public static void main(String args[]){
+        // Scanner sc = new Scanner(System.in);
+        
+        // LocalDate tod = LocalDate.now();
+
+        
+        
+        System.out.println("13");
+        System.out.println("haioeod1");
+        
+    }
+}
