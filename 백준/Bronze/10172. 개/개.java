7fd9d68d63508c7fb1588056bf6e6699b3c54fc1
jyeoniop <wldud128iop@gmail.com>
2025-11-04 17:08:21 +0900

    [Bronze V] Title: 개, Time: 96 ms, Memory: 14012 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.time.LocalDate;
+
+public class Main{
+    public static void main(String args[]){
+        // Scanner sc = new Scanner(System.in);
+        
+        // LocalDate tod = LocalDate.now();
+
+        
+        
+        System.out.println("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|");
+        
+    }
+}
