e1f952fa09eb29ea050221b1178570d840f1465f
jyeoniop <wldud128iop@gmail.com>
2025-11-04 17:22:10 +0900

    [Bronze V] Title: 꼬마 정민, Time: 168 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,17 @@
+import java.util.*;
+// import java.time.LocalDate;
+
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long c = sc.nextLong();
+
+        
+        
+        System.out.print(a+b+c);
+        
+    }
+}
