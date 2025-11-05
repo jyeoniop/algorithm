1eb9664532825e9f50c091e5a8151f60411df5bb
jyeoniop <wldud128iop@gmail.com>
2025-11-05 18:35:27 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 104 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+// import java.time.LocalDate;
+
+public class Main{
+    public static void main(String args[]) throws Exception{
+
+
+        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        // StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        String word = br.readLine();
+        int len = word.length();
+
+        System.out.println(len);
+
+       
+    }
+}
