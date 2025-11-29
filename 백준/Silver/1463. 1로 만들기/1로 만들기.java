bcbbfec8bae7115045e5844b8b6747063294a42d
jyeoniop <wldud128iop@gmail.com>
2025-11-29 20:02:47 +0900

    [Silver III] Title: 1로 만들기, Time: 116 ms, Memory: 18156 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+// import javax.sound.midi.SysexMessage;
+
+import java.io.*;
+
+public class Main{
+    public static void main(String args[]) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        int[] dp = new int[N+1];
+        dp[1] = 0;
+
+        for(int i=2;i<=N;i++){
+            dp[i] = dp[i-1]+1;
+            if(i%2==0){
+                dp[i] = Math.min(dp[i/2]+1, dp[i]);
+            }
+            if(i%3==0){
+                dp[i] = Math.min(dp[i/3]+1, dp[i]);
+            }
+        }
+
+
+        
+
+
+        System.out.println(dp[N]);
+
+        
+    }
+
+    
+    }
+
+
+
+        
+     
