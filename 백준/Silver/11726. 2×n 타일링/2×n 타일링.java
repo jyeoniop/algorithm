eabd56b51b0c0c126bb9f65995fed5142525ef5e
jyeoniop <wldud128iop@gmail.com>
2025-12-03 18:05:03 +0900

    [Silver III] Title: 2×n 타일링, Time: 108 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+// import javax.sound.midi.SysexMessage;
+
+import java.io.*;
+
+public class Main{
+ 
+    public static void main(String args[]) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        
+        int N = Integer.parseInt(br.readLine());
+        long[] rectan = new long[N+1];
+        if(N>0){
+            rectan[1] = 1;
+        }
+        if(N>1){rectan[2] = 2;}
+        if(N>2){
+            for(int i=3;i<=N;i++){
+                rectan[i] = (rectan[i-2]+rectan[i-1])%10007;
+            }
+
+        }
+
+
+
+
+        
+        System.out.println(rectan[N]);
+
+        
+    }
+
+    
+    }
