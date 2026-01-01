5977c15dda795385abc43a6fbb43ac816ca89098
jyeoniop <wldud128iop@gmail.com>
2026-01-01 16:42:13 +0900

    [Silver V] Title: 덩치, Time: 104 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.java
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -1,45 +1,43 @@
+import java.io.*;
+import java.lang.*;
 import java.util.*;
 
-// import javax.sound.midi.SysexMessage;
-
-import java.io.*;
-// import java.time.LocalDate;
-// import java.lang.reflect.Array;
-
 public class Main{
-
+ 
     public static void main(String args[]) throws Exception{
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st ;
+        StringTokenizer st;
 
-        int N = Integer.parseInt(br.readLine());
-        
-        int dunchi[][] = new int[N][N];
+        int N  = Integer.parseInt(br.readLine());
+        int[][] person = new int[N][2];
         for(int i=0;i<N;i++){
-            st = new StringTokenizer(br.readLine());
-            dunchi[i][0] = Integer.parseInt(st.nextToken());
-            dunchi[i][1] = Integer.parseInt(st.nextToken());
+            st= new StringTokenizer(br.readLine()," ");
+            int we = Integer.parseInt(st.nextToken());
+            int hi = Integer.parseInt(st.nextToken());
+            person[i][0] = we;
+            person[i][1] = hi;
+
         }
-        int k;
-        for(int i =0;i<N;i++){
-            k =0;
+        int[] countp = new int[N];
+        for(int i=0;i<N;i++){
+            int count = 1;
             for(int j=0;j<N;j++){
                 if(i==j){continue;}
-                else{
-                    if(dunchi[i][0]<dunchi[j][0] && dunchi[i][1]<dunchi[j][1]){
-                        k ++;
-                    }
+                if(person[i][0]<person[j][0]&&person[i][1]<person[j][1]){
+                    count++;
                 }
             }
-            System.out.print(String.valueOf(k+1));
-            System.out.print(" ");
+            countp[i] = count;
+        }
+        for (int i : countp) {
+            sb.append(i).append("\n");
         }
 
+
+
+        System.out.println(sb.toString());
+    
     }
-    }
-
-
-
-
+}
