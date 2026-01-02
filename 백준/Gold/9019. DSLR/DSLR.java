ce977304c0583cc51f022d7754b84ae13a43c521
jyeoniop <wldud128iop@gmail.com>
2026-01-02 21:30:46 +0900

    [Gold IV] Title: DSLR, Time: 3740 ms, Memory: 303276 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.java
+++ 백준/Gold/9019. DSLR/DSLR.java
@@ -1,45 +1,54 @@
 import java.io.*;
 import java.lang.*;
 import java.util.*;
-import java.util.function.IntUnaryOperator;
 public class Main{
     static boolean[] visited;
     static int start;
     static int end;
-    static char[] cmdChar = {'D', 'S', 'L', 'R'};
-    static String[] path;
     static StringBuilder sb = new StringBuilder();
-    public static IntUnaryOperator[] dslr(int num){
-        IntUnaryOperator[] dslr = new IntUnaryOperator[4];
-        dslr[0] = n -> (2*num>9999)?(2*num)%10000:2*num;
-        dslr[1] = n -> (num==0)?9999:num-1;
-        dslr[2] = n -> num%1000*10+num/1000;
-        dslr[3] = n -> num%10*1000+num/10;
-        return dslr;
+    static class Node{
+        int val;
+        String comnmand;
+
+        public Node(int val,String comnmand ) {
+            this.comnmand = comnmand;
+            this.val = val;
+        }
     }
+
     public static void bfs(int num){
-        Queue<Integer> q = new LinkedList<>();
-        q.add(num);
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(num, ""));
         visited[num]=true;
-        path[num] = "";
 
         while(!q.isEmpty()){
-            int n = q.poll();
-            if(n==end){
-                sb.append(path[n]).append("\n");
+            Node now = q.poll();
+            if(now.val==end){
+                sb.append(now.comnmand).append("\n");
                 return;
             }
-            IntUnaryOperator[] op = dslr(n);
-            int dslrresult =0;
-
-            for(int i=0;i<4;i++){
-                dslrresult = op[i].applyAsInt(0);
-                if(!visited[dslrresult]){
-                    visited[dslrresult] = true;
-                    path[dslrresult] = path[n]+cmdChar[i];
-                    q.add(dslrresult);
+            
+            int d = (now.val * 2) % 10000;
+            if(!visited[d]){
+                    visited[d] = true;
+                    q.add(new Node(d, now.comnmand+"D"));
                 }
-            }
+            int s = (now.val==0)?9999:now.val-1;
+            if(!visited[s]){
+                    visited[s] = true;
+                    q.add(new Node(s, now.comnmand+"S"));
+                }
+            int l = now.val%1000*10+now.val/1000;
+            if(!visited[l]){
+                    visited[l] = true;
+                    q.add(new Node(l, now.comnmand+"L"));
+                }
+            int r = now.val%10*1000+now.val/10;
+            if(!visited[r]){
+                    visited[r] = true;
+                    q.add(new Node(r, now.comnmand+"R"));
+                }
+            
         }
 
     }
@@ -58,7 +67,6 @@ public class Main{
             start = Integer.parseInt(st.nextToken());
             end = Integer.parseInt(st.nextToken());
             visited = new boolean[10000];
-            path = new String[10000];
     
             bfs(start);
 
