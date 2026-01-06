8e53b74dc6f87379d0f570f8c0d473dc859fda1c
jyeoniop <wldud128iop@gmail.com>
2026-01-06 16:12:14 +0900

    [Silver III] Title: N과 M (2), Time: 100 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+public class Main{
+
+    static int N;
+    static int M;
+    static StringBuilder sb = new StringBuilder();
+    static int[] result;
+    static boolean[] visitied;
+    public static void dfs(int depth, int start){
+
+        if(depth==M){
+
+            for (int i : result) {
+                sb.append(i).append(" ");
+                
+            }sb.append("\n");
+            return;
+        }
+        for(int i=start;i<=N;i++){
+            if(!visitied[i]){
+                visitied[i] = true;
+                result[depth] =i;
+                dfs(depth+1, i+1);
+                visitied[i] = false;
+
+            }
+        }
+    }
+    public static void main(String args[]) throws Exception{
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        // StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        
+        result = new int[M];
+        visitied = new boolean[N+1];
+            dfs(0,1);
+
+
+
+
+        System.out.println(sb.toString());
+    
+    }
+}
