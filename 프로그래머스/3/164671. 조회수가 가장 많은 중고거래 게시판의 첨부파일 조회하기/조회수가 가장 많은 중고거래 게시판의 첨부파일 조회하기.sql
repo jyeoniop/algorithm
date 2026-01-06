fb48729ddaf84fe2139266380346d2ac45d317e3
jyeoniop <wldud128iop@gmail.com>
2026-01-06 11:33:30 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,5 @@
+-- 코드를 입력하세요
+SELECT concat('/home/grep/src/', board_id, '/',file_id, file_name, file_ext) as file_path
+from USED_GOODS_FILE 
+where board_id = (select board_id from USED_GOODS_BOARD order by views desc limit 1)
+order by file_id desc
