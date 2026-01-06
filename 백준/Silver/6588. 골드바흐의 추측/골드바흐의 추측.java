38daf97ffaac021e5c0e31365104b462937d1de1
jyeoniop <wldud128iop@gmail.com>
2026-01-06 14:31:00 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 496 ms, Memory: 37616 KB -BaekjoonHub

---
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+public class Main{
+    public static void main(String args[]) throws Exception{
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        
+        boolean[] isnotprime = new boolean[1_000_001];
+        isnotprime[0] = isnotprime[1] = true;
+        for(int i = 2;i<=Math.sqrt(1_000_000);i++){
+            if(!isnotprime[i]){
+                for(int j=i*i;j<=1_000_000;j+=i){
+                        isnotprime[j] = true;
+    
+                }
+            }
+        }
+
+        while(true){
+            int N = Integer.parseInt(br.readLine());
+            if(N==0){break;}
+
+            boolean chk = true;
+            for (int i=3;i<=N/2;i+=2) {
+                if(!isnotprime[N-i]&&!isnotprime[i]){
+                    sb.append(N).append(" = ").append(i).append(" + ").append(N-i).append("\n");
+                    chk  = false;
+                    break;
+                }
+            }
+            if(chk){
+                sb.append( "Goldbach's conjecture is wrong.\n");
+            }
+            
+
+
+        }
+
+
+        System.out.println(sb.toString());
+    
+    }
+}
