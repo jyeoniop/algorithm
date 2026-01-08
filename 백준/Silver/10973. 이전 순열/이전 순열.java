762d91e18b7f829c10619b510e46fd2e96746892
jyeoniop <wldud128iop@gmail.com>
2026-01-08 18:48:35 +0900

    [Silver III] Title: 이전 순열, Time: 284 ms, Memory: 17836 KB -BaekjoonHub

---
+++ 백준/Silver/10973. 이전 순열/이전 순열.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+public class Main{
+    static int[] nums;
+    public static void swap(int a, int b){
+        int temp= nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+
+    public static void main(String args[]) throws Exception{
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        
+
+       int N = Integer.parseInt(br.readLine());
+       nums = new int[N];
+       
+       st = new StringTokenizer(br.readLine(), " ");
+       for(int i=0;i<N;i++){
+        nums[i] = Integer.parseInt(st.nextToken());
+       }
+
+       int targetindex =-1;
+       for(int i=N-1;i>0;i--){
+        if(nums[i]<nums[i-1]){
+            targetindex = i-1;
+            break;
+        }
+       }
+
+       if(targetindex!=-1){
+
+        int maxvalue = 0;
+        int maxindex = -1;
+        for(int i=N-1;i>targetindex;i--){
+            if(maxvalue<nums[i]&&nums[targetindex]>nums[i]){
+                maxvalue = nums[i];
+                maxindex = i;
+            }
+        }
+        swap(targetindex, maxindex);
+           
+        int len = N-targetindex-1;
+
+        for(int i=1;i<=Math.floorDiv(len, 2);i++){
+            swap(targetindex+i, N-i);
+        }
+
+           for (int i : nums) {
+            System.out.print(i+" ");
+           }
+       }else{
+        System.out.println("-1");
+       }
+       
+
+
+
+
+
+
+        // System.out.println(sb.toString());
+    
+    }
+}
