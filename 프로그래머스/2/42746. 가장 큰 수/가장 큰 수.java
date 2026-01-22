c38f2a4e944c3cbdb9415166d538009526fe674c
jyeoniop <wldud128iop@gmail.com>
2026-01-22 20:59:15 +0900

    [level 2] Title: 가장 큰 수, Time: 230.43 ms, Memory: 138 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -6,9 +6,9 @@ class Solution {
         for(int i=0;i<numbers.length;i++){
             str[i] = String.valueOf(numbers[i]);
         }
-        Arrays.sort(str, (a, b) -> (b + a).compareTo(a + b));
-        answer = String.join("", str);
-        if(answer.charAt(0)=='0'){answer = "0";}
+        Arrays.sort(str, (a,b)->(b+a).compareTo(a+b));
+        answer = String.join("",str);
+        if(answer.charAt(0)=='0')answer ="0";
         return answer;
     }
 }
