e357d2946a6eb9cb600a7a1f51424954a8a7e0ed
jyeoniop <wldud128iop@gmail.com>
2026-01-23 16:01:24 +0900

    [level 4] Title: 도둑질, Time: 27.46 ms, Memory: 104 MB -BaekjoonHub

---
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int[] money) {
+        int answer = 0;
+        int n = money.length;
+        
+        // 1. 첫 번째 집을 무조건 터는 경우 (마지막 집 제외)
+        int[] dp1 = new int[n];
+        dp1[0] = money[0];
+        dp1[1] = Math.max(money[0], money[1]);
+        for (int i = 2; i < n - 1; i++) {
+            dp1[i] = Math.max(dp1[i - 1], dp1[i - 2] + money[i]);
+        }
+        
+        // 2. 첫 번째 집을 안 터는 경우 (마지막 집 포함 가능)
+        int[] dp2 = new int[n];
+        dp2[0] = 0;
+        dp2[1] = money[1];
+        for (int i = 2; i < n; i++) {
+            dp2[i] = Math.max(dp2[i - 1], dp2[i - 2] + money[i]);
+        }
+        return Math.max(dp1[n - 2], dp2[n - 1]);
+        // return answer;
+    }
+}
