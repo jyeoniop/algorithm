610b5283c47a660b0995ea8ec5f4afcfddfc554b
jyeoniop <wldud128iop@gmail.com>
2026-02-27 19:55:29 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
+++ 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -1,6 +1,14 @@
-SELECT cart_id
-from CART_PRODUCTS 
-where name in ('Milk', 'Yogurt')
+# select cart_id
+# from CART_PRODUCTS 
+# where name in("Yogurt", "Milk")
+# group by cart_id
+# having count(distinct name)=2
+# order by cart_id asc
+
+
+select cart_id
+from cart_products
+where name in('Milk','Yogurt')
 group by cart_id
 having count(distinct name)=2
-order by cart_id asc
+order by id asc;
