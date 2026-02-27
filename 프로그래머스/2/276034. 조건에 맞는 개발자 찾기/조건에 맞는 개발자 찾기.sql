67251bc08995ff86ff3f08987ef53da2753159bb
jyeoniop <wldud128iop@gmail.com>
2026-02-27 20:59:37 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -4,7 +4,21 @@
 # order by d.id asc
 
 
-select distinct d.id, d.email, d.first_name, d.last_name
-from developers as d join skillcodes as s on (d.skill_code&s.code)>0
-where s.name = 'C#' or s.name='Python'
-order by d.id asc;
+# select distinct d.id, d.email, d.first_name, d.last_name
+# from developers as d join skillcodes as s on (d.skill_code&s.code)>0
+# where s.name = 'C#' or s.name='Python'
+# order by d.id asc;
+
+
+select id, email, first_name, last_name
+from developers as d
+where (d.skill_code&(select sum(code) from skillcodes where name="C#" or name="Python"))>0
+order by id asc;
+
+
+
+
+
+
+
+
