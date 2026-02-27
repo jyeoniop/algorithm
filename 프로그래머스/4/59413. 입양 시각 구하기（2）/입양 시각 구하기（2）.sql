bd17797710f72be781bd58ac543547607cda7b7e
jyeoniop <wldud128iop@gmail.com>
2026-02-27 22:02:47 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,10 +1,22 @@
-with recursive time as(
-    select 0 as hour
-    union all
-    select hour+1
-    from time
-    where hour<23
-)
-select time.hour as hour, count(case when animal_outs.animal_id is null then null else 1 end) as count
-from time left join animal_outs on time.hour=hour(animal_outs.datetime)
-group by time.hour
+# with recursive time as(
+#     select 0 as hour
+#     union all
+#     select hour+1
+#     from time
+#     where hour<23
+# )
+# select time.hour as hour, count(case when animal_outs.animal_id is null then null else 1 end) as count
+# from time left join animal_outs on time.hour=hour(animal_outs.datetime)
+# group by time.hour
+
+with recursive time as(select 0 as hour 
+union all
+select hour+1
+from time
+where hour<23)
+select t.hour, count(case when o.animal_id is not null then 1 else null end) as count
+from time as t left join ANIMAL_OUTS as o on t.hour= hour(o.datetime)
+group by t.hour
+order by t.hour
+
+
