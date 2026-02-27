94c473db3861f70925931bfff8ee34e5728b1628
jyeoniop <wldud128iop@gmail.com>
2026-02-27 21:09:02 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -32,15 +32,39 @@
 
 
 
-with getgrade as(
-    select distinct 
-    case when (skill_code&(select sum(code) from skillcodes as sk where sk.category='Front End')>0) and (skill_code&(select code from skillcodes as sk where sk.name='Python')>0) then 'A'
-when skill_code&(select code from skillcodes as sk where sk.name='C#')>0 then 'B'
-when skill_code&(select sum(code) from skillcodes as sk where sk.category='Front End')>0 then 'C'
-else null end as grade,
-id, email
-from DEVELOPERS )
-select *
+# with getgrade as(
+#     select distinct 
+#     case when (skill_code&(select sum(code) from skillcodes as sk where sk.category='Front End')>0) and (skill_code&(select code from skillcodes as sk where sk.name='Python')>0) then 'A'
+# when skill_code&(select code from skillcodes as sk where sk.name='C#')>0 then 'B'
+# when skill_code&(select sum(code) from skillcodes as sk where sk.category='Front End')>0 then 'C'
+# else null end as grade,
+# id, email
+# from DEVELOPERS )
+# select *
+# from getgrade
+# where grade is not null
+# order by grade asc, id asc
+
+with getgrade as(select case 
+when (skill_code&(select sum(code) from skillcodes where category='Front End')>0&&
+      skill_code&(select code from skillcodes where name='Python')>0) then 'A'
+when skill_code&(select code from skillcodes where name='C#')>0 then 'B'
+when skill_code&(select sum(code) from skillcodes where category='Front End')>0 then 'C'
+else null end as grade, id, email
+from developers 
+)
+select grade, id, email
 from getgrade
 where grade is not null
-order by grade asc, id asc
+order by grade asc, id asc;
+
+
+
+
+
+
+
+
+
+
+
