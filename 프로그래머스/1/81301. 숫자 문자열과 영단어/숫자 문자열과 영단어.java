4afa9dde135e1d89c23e983528a109d733855b1c
jyeoniop <wldud128iop@gmail.com>
2026-03-03 17:27:28 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.17 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,29 +1,35 @@
+import java.util.*;
 class Solution {
     public int solution(String s) {
-        int answer = 0;
-        
-        String[] number = {
-            "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
-        };
-
-        String chk = "";
+        StringBuilder sb= new StringBuilder();
+        Character[] str = new Character[s.length()];
         for(int i=0;i<s.length();i++){
-            if(!Character.isDigit(s.charAt(i))){
-                chk = "";
-                chk = String .valueOf(s.charAt(i))+String.valueOf(s.charAt(i+1));
-                for(int j=0;j<10;j++){
-                    if(number[j].startsWith(chk)){
-                        answer = answer*10+j;
-                        i += number[j].length()-1;
-                    }
-                }
-                
-            }else{
-                answer = answer*10+(s.charAt(i)-'0');
-            }
-                
+            str[i] = s.charAt(i);
         }
-        
+        for(int i=0;i<s.length();){
+            
+            if(Character.isDigit(str[i])){
+                sb.append(str[i]);
+                i++;
+            }else{
+                // String word = s.charAt(i)+s.charAt(i+1)+s.charAt(i+2)+"";
+
+                if(str[i]=='z'){sb.append(0); i+=4;}
+                else if(str[i]=='o'){sb.append(1); i+=3;}
+                else if(str[i]=='t'&&str[i+1]=='w'){sb.append(2); i+=3;}
+                else if(str[i]=='t'&&str[i+1]=='h'){sb.append(3); i+=5;}
+                else if(str[i]=='f'&&str[i+1]=='o'){sb.append(4); i+=4;}
+                else if(str[i]=='f'&&str[i+1]=='i'){sb.append(5); i+=4;}
+                else if(str[i]=='s'&&str[i+1]=='i'){sb.append(6); i+=3;}
+                else if(str[i]=='s'&&str[i+1]=='e'){sb.append(7); i+=5;}
+                else if(str[i]=='e'){sb.append(8); i+=5;}
+                else {sb.append(9); i+=4;}
+                
+                
+            }
+        }
+
+        int answer = Integer.parseInt(sb.toString());
         return answer;
     }
 }
