49e159be2ddf74438e89397c3e290938885455f6
jyeoniop <wldud128iop@gmail.com>
2026-03-12 17:54:57 +0900

    [level 1] Title: 명예의 전당 (1), Time: 2.67 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -2,19 +2,14 @@ import java.util.*;
 class Solution {
     public int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
-        
-         Queue<Integer> q = new PriorityQueue<>();
-        int[] result = new int[score.length];
-
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         for(int i=0;i<score.length;i++){
-            q.add(score[i]);
-            if((i+1)>k) {
-                q.poll();
+            pq.add(score[i]);
+            while(pq.size()>k){
+                pq.poll();
             }
-            result[i] = q.peek();
+            answer[i] = pq.peek();
         }
-        
-        answer = result;
         
         return answer;
     }
