f14f662eb5e9e22a32ff24ac146b5937de7dd6b4
jyeoniop <wldud128iop@gmail.com>
2026-03-27 16:34:37 +0900

    [level 1] Title: 기사단원의 무기, Time: 115.83 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -2,24 +2,20 @@ class Solution {
     public int solution(int number, int limit, int power) {
         int answer = 0;
         
-        int[] pnum = new int[number+1];
+        int[] subcount = new int[number+1];
+        int count = 0;
         for(int i=1;i<=number;i++){
+            count = 0;
             for(int j=1;j*j<=i;j++){
                 if(i%j==0){
-                    pnum[i]++;
-                    if(j*j!=i)pnum[i]++;
+                    count++;
+                    if(j*j!=i)count++;
                 }
-                
-                }
+            }subcount[i] = count<=limit?count:power;
         }
         
-        for(int p:pnum){
-
-            if(p>limit){
-                answer += power;
-            }else{
-                answer += p;
-            }
+        for(int sub:subcount){
+            answer += sub;
         }
         
         return answer;
