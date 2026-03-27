4ec02f8f89eb6e049d3a764cacdc3210cecfab25
jyeoniop <wldud128iop@gmail.com>
2026-03-27 16:42:25 +0900

    [level 1] Title: 2016년, Time: 0.02 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -1,17 +1,33 @@
-import java.time.*;
-import java.time.format.*;
 class Solution {
     public String solution(int a, int b) {
         String answer = "";
         
-        String astr = a<10? "0"+a: a+"";
-        String bstr = b<10? "0"+b: b+"";
-        String date = "2016"+astr+bstr;
-        System.out.println(date);
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyyMMdd");
-        LocalDate localDate = LocalDate.parse(date, dateTimeFormatter);
+        int days = -1;
+        if(a>1)days+=31;
+        if(a>2)days+=29;
+        if(a>3)days+=31;
+        if(a>4)days+=30;
+        if(a>5)days+=31;
+        if(a>6)days+=30;
+        if(a>7)days+=31;
+        if(a>8)days+=31;
+        if(a>9)days+=30;
+        if(a>10)days+=31;
+        if(a>11)days+=30;
+        
+        days+=b;
+        days%=7;
+        switch (days){
+            case 0:return "FRI";
+            case 1:return "SAT";
+            case 2:return "SUN";
+            case 3:return "MON";
+            case 4:return "TUE";
+            case 5:return "WED";
+            case 6:return "THU";
+        }
 
-        answer = localDate.getDayOfWeek().name().substring(0,3);
+        
         return answer;
     }
 }
