6ffdea0cc436b4cf38cbfa2bc7fd7126200d6aa9
jyeoniop <wldud128iop@gmail.com>
2026-03-29 22:59:06 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 12.43 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -1,30 +1,32 @@
 class Solution {
     public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
-        String answer = "";
+        int trans_len = Integer.parseInt(video_len.substring(0,2))*60+Integer.parseInt(video_len.substring(3,5));
+        int trans_pos = Integer.parseInt(pos.substring(0,2))*60+Integer.parseInt(pos.substring(3,5));
+        int trans_start = Integer.parseInt(op_start.substring(0,2))*60+Integer.parseInt(op_start.substring(3,5));
+        int trans_end = Integer.parseInt(op_end.substring(0,2))*60+Integer.parseInt(op_end.substring(3,5));
         
-        int len = Integer.parseInt(video_len.split(":")[0])*60+Integer.parseInt(video_len.split(":")[1]);
-        int intpos = Integer.parseInt(pos.split(":")[0])*60+Integer.parseInt(pos.split(":")[1]);
-        int start = Integer.parseInt(op_start.split(":")[0])*60+Integer.parseInt(op_start.split(":")[1]);
-        int end = Integer.parseInt(op_end.split(":")[0])*60+Integer.parseInt(op_end.split(":")[1]);
-
-        for (String com : commands) {
-            if(intpos>=start&&intpos<=end){
-                intpos = end;
-            }
-            if(com.equals("next")){
-                intpos +=10;
-                if(intpos>len)intpos = len;
+        int count = commands.length;
+        int pollcount = 0;
+        while(count!=pollcount){
+            if(trans_pos>=trans_start&&trans_pos<trans_end){
+                trans_pos = trans_end;
             }else{
-                intpos -=10;
-                if(intpos<0)intpos = 0;
+                if(commands[pollcount++].equals("prev")){
+                    trans_pos = trans_pos<=10?0:trans_pos-10;
+                }else{
+                    trans_pos = trans_pos>=(trans_len-10)?trans_len:trans_pos+10;
+                }
             }
+            
         }
-        if(intpos>=start&&intpos<=end){
-                intpos = end;
-            }
-        answer = String.format("%02d", intpos/60)+":"+String.format("%02d",intpos%60);
+        if(trans_pos>=trans_start&&trans_pos<=trans_end){
+                trans_pos = trans_end;
+        }
         
+        int hour = trans_pos/60;
+        int min = trans_pos-hour*60;
         
+        String answer = String.format("%02d", hour)+":"+String.format("%02d", min);
         return answer;
     }
 }
