7fabec8c55e66a87c8b052233c685299cf064f3a
jyeoniop <wldud128iop@gmail.com>
2026-04-01 17:35:03 +0900

    [level 3] Title: 네트워크, Time: 0.82 ms, Memory: 90.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,25 +1,51 @@
 import java.util.*;
 class Solution {
+    static ArrayList<ArrayList<Integer>> arr;
+    static boolean[] visited;
+    static int n;
+    static int count=0;
+    void bfs(int i){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(i);
+        visited[i] = true;
+        
+        while(!q.isEmpty()){
+            int cur = q.poll();
+            for(int a:arr.get(cur)){
+                if(!visited[a]){
+                 visited[a] = true;
+                 q.add(a);
+                }
+            }
+            
+            
+        }
+    }
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-        for(int i=0;i<computers.length;i++){
-            graph.add(new ArrayList<>());
+        this.n=n;
+        arr = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            arr.add(new ArrayList<>());
         }
-        for(int i=0;i<computers.length;i++){
+        
+        for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(i!=j&&computers[i][j]==1){
-                    graph.get(i).add(j);
+                    arr.get(i).add(j);
                 }
             }
         }
-        for(int i=0;i<graph.size();i++){
-            System.out.println("i : "+i);
-            for(int j:graph.get(i)){
-                System.out.println(j);
+        
+        visited = new boolean[n];
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                count++;
+                bfs(i);        
             }
         }
+        
+        answer = count;
         
         return answer;
     }
