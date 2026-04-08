4a9f3e81491823a10e4477a893232eea25877c26
jyeoniop <wldud128iop@gmail.com>
2026-04-08 15:53:26 +0900

    [level 4] Title: 징검다리, Time: 39.74 ms, Memory: 97 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.java
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -1,29 +1,28 @@
 import java.util.*;
-    class Solution {
+class Solution {
     public int solution(int distance, int[] rocks, int n) {
         int answer = 0;
         Arrays.sort(rocks);
         
-        int left = 1;
+        int left= 1;
         int right = distance;
         
         while(left<=right){
             int mid = (left+right)/2;
             
-            int removecount = 0;
+            int removecnt = 0;
             int prerock = 0;
             
             for(int i=0;i<rocks.length;i++){
                 if(rocks[i]-prerock<mid){
-                    removecount++;
+                    removecnt++;
                 }else{
                     prerock = rocks[i];
                 }
+            }if(distance-prerock<mid){
+                removecnt++;
             }
-            if(distance-prerock<mid){
-                removecount++;
-            }
-            if(removecount<=n){
+            if(removecnt<=n){
                 answer = mid;
                 left = mid+1;
             }else{
