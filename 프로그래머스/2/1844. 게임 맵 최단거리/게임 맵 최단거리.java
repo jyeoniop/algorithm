7b99b8716d83fd50475dbe77de482478404c8826
jyeoniop <wldud128iop@gmail.com>
2026-04-27 09:51:55 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.80 ms, Memory: 58.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,32 +1,29 @@
 import java.util.*;
 class Solution {
-    static int N;
-    static int M;
-    static boolean[][] visited;
-    static int[][] count;
-    static int[][] map;
-    
-    public void bfs(int[] start){
+    public static int[][] result;
+    public static boolean[][] visited;
+    public static int[][] maps;
+    public static void bfs(){
         Queue<int[]> q = new LinkedList<>();
-        int[] dx = {1,0,-1,0};
-        int[] dy = {0,1,0,-1};
+        int[] dx = {0,1,0,-1};
+        int[] dy = {1,0,-1,0};
         
-        q.add(start);
-        count[0][0] = 1;
+        q.add(new int[]{0,0});
+        visited[0][0] = true;
+        result[0][0] = 1;
+        
         while(!q.isEmpty()){
             int[] cur = q.poll();
             int curx = cur[0];
             int cury = cur[1];
-            
             for(int i=0;i<4;i++){
-                int netx = curx + dx[i];
-                int nety = cury + dy[i];
-                if(netx>=0&&netx<N&&nety>=0&&nety<M){
-                    if(!visited[netx][nety]&&map[netx][nety]!=0){
-                        visited[netx][nety]= true;
-                        count[netx][nety] = count[curx][cury]+1;
-                        q.add(new int[]{netx,nety});
-                        
+                int ntx = curx+dx[i];
+                int nty = cury+dy[i];
+                if(ntx>=0&&nty<maps[0].length&&nty>=0&&ntx<maps.length){
+                    if(!visited[ntx][nty]&&maps[ntx][nty]==1){
+                        visited[ntx][nty] = true;
+                        q.add(new int[]{ntx, nty});
+                        result[ntx][nty] = result[curx][cury]+1;
                     }
                 }
             }
@@ -34,22 +31,11 @@ class Solution {
     }
     public int solution(int[][] maps) {
         int answer = 0;
-        map = maps;
-        N = maps.length;
-        M = maps[0].length;
-        visited = new boolean[N+1][M+1];
-        count = new int[N][M];
-        
-        bfs(new int[]{0,0});
-        
-        // for(int i=0;i<N;i++){
-        //     for(int j=0;j<M;j++){
-        //         System.out.print(count[i][j]+" ");
-        //     }System.out.println();
-        // }
-        if(count[N-1][M-1]==0){answer = -1;}
-        else{answer = count[N-1][M-1];}
-
+        result = new int[maps.length][maps[0].length];
+        visited = new boolean[maps.length+1][maps[0].length+1];
+        this.maps = maps;
+        bfs();
+        answer = result[maps.length-1][maps[0].length-1]==0?-1:result[maps.length-1][maps[0].length-1];
         return answer;
     }
 }
