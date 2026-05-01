c34fbfcefb2944ff3f2ba32bc4cf21e9956507f7
jyeoniop <wldud128iop@gmail.com>
2026-05-01 17:52:35 +0900

    [level 2] Title: 큰 수 만들기, Time: 77.44 ms, Memory: 117 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    public String solution(String number, int k) {
+        Deque<Integer> stk = new ArrayDeque<>();
+        
+        int index = 0;
+        while(index<number.length()){
+            if(!stk.isEmpty()&&stk.peek()<(number.charAt(index)-'0')&&k>0){
+                while((!stk.isEmpty()&&stk.peek()<(number.charAt(index)-'0')&&k>0)){
+                    stk.pop();
+                    k--;    
+                }
+            }
+            stk.push((number.charAt(index)-'0'));
+            index++;
+        }
+        while(k > 0) {
+            stk.pop();
+            k--;
+        }
+        StringBuilder sb = new StringBuilder();
+        while(!stk.isEmpty())sb.append(stk.pop());
+        sb.reverse();
+        String answer = sb.toString();
+        return answer;
+    }
+}
